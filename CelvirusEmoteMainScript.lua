9bc67b19372cd093597b9c771071e250a3481948
ㅤ <algaryalga@gmail.com>
2025-10-29 21:39:35 +0700

    Add new emotes and update existing animations

--- CelvirusEmoteMainScript.lua
+++ CelvirusEmoteMainScript.lua
@@ -1,8 +1,18 @@
-local Emotes = loadstring(game:HttpGet("https://raw.githubusercontent.com/AlgariBot/roblox/refs/heads/main/CelvirusEmote.lua"))()[1]
+local main = loadstring(game:HttpGet("https://raw.githubusercontent.com/AlgariBot/roblox/refs/heads/main/CelvirusEmote.lua"))()
+
+local Emotes = main[1]
+local namebox = main[2]
 
 Emotes.animate({
 	name=[[Blowjob <font color="rgb(190,190,190)">[Self]</font>]],targetBox=namebox,
 	anim1="74543120303961",anim2="0",anim3="0",anim4="0",anim5="0",anim6="0",
+	offset1=CFrame.new(0,0,-1.5),offset2=CFrame.new(0,0,-1.5),
+	rotX=0,rotY=180,rotZ=0,speed=20,toggleDelay=0,delay=0,speedMult=10
+})
+
+Emotes.animate({
+	name=[[Blowjob 2 <font color="rgb(190,190,190)">[Self]</font>]],targetBox=namebox,
+	anim1="104741679253066",anim2="0",anim3="0",anim4="0",anim5="0",anim6="0",
 	offset1=CFrame.new(0,0,-1.5),offset2=CFrame.new(0,0,-1.5),
 	rotX=0,rotY=180,rotZ=0,speed=20,toggleDelay=0,delay=0,speedMult=10
 })
@@ -26,4 +36,11 @@ Emotes.animate({
 	anim1="90696131349427",anim2="0",anim3="0",anim4="0",anim5="0",anim6="0",
 	offset1=CFrame.new(0,0,-0.5),offset2=CFrame.new(0,0,-1.4),
 	rotX=0,rotY=0,rotZ=0,speed=10,toggleDelay=0.5,delay=5,speedMult=0
+})
+
+Emotes.animate({
+	name=[[Leg up <font color="rgb(190,190,190)">[Self]</font>]],targetBox=namebox,
+	anim1="98592031933521",anim2="0",anim3="0",anim4="0",anim5="0",anim6="0",
+	offset1=CFrame.new(0,0,-1),offset2=CFrame.new(0,0,-1.7),
+	rotX=0,rotY=0,rotZ=0,speed=10,toggleDelay=0.5,delay=0,speedMult=1
 })

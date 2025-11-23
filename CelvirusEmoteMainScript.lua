14752ea828f7150ae3b90a32d3635b7a4c8711e0
ㅤ <algaryalga@gmail.com>
2025-11-23 15:39:23 +0700

    Update emote labels and names in script

--- CelvirusEmoteMainScript.lua
+++ CelvirusEmoteMainScript.lua
@@ -3,7 +3,7 @@ local main = loadstring(game:HttpGet("https://raw.githubusercontent.com/AlgariBo
 local Emotes = main[1]
 local namebox = main[2]
 
-Emotes.Label({text="Self animations"})
+Emotes.Label({text="Local player animations"})
 
 Emotes.animate({
 	name=[[Blowjob <font color="rgb(190,190,190)">[Self]</font>]],targetBox=namebox,
@@ -62,16 +62,16 @@ Emotes.animate({
 })
 
 Emotes.animate({
-	name=[[Yoga <font color="rgb(190,190,190)">[Self]</font>]],targetBox=namebox,
+	name=[[Yoga (bad) <font color="rgb(190,190,190)">[Self]</font>]],targetBox=namebox,
 	anim1="111755980245826",anim2="0",anim3="0",anim4="0",anim5="0",anim6="0",
 	offset1=CFrame.new(0,0,-2.2),offset2=CFrame.new(0,0,-1.3),
 	rotX=0,rotY=0,rotZ=0,speed=10,toggleDelay=0.5,delay=0,speedMult=1
 })
 	
-Emotes.Label({text="Target Animations"})
+Emotes.Label({text="Target Player Animations"})
 
 Emotes.animate({
-	name=[[Ejaculate <font color="rgb(190,190,190)">[Target]</font>]],targetBox=namebox,
+	name=[[idk <font color="rgb(190,190,190)">[Target]</font>]],targetBox=namebox,
 	anim1="91149775165340",anim2="0",anim3="0",anim4="0",anim5="0",anim6="0",
 	offset1=CFrame.new(1,0,-2.8),offset2=CFrame.new(1,0,-2.8),
 	rotX=0,rotY=-88,rotZ=0,speed=20,toggleDelay=0,delay=0,speedMult=1.4
